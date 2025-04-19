e1e40fb1f55d623944862c9de2547c08ef478f37
YangyangMiao <yangyangmiao666@outlook.com>
2025-04-19 11:38:47 +0800

    pom文件调整

---
+++ mcp-client-demo/src/main/java/com/ustc/myy/mcpclientdemo/McpClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ustc.myy.mcpclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpClientDemoApplication.class, args);
+    }
+
+}
