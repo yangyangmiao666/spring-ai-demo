dc18967dd30abba2aba91fae4576cdab019e6e6a
YangyangMiao <yangyangmiao666@icloud.com>
2025-06-29 18:02:57 +0800

    refactor(alibaba-graph): 移除 React 相关代码，添加简单图控制器和节点
    - 删除了 ReactAutoconfiguration 和 ReactController 类
    - 新增了 GraphConfiguration、SimpleGraphController 和 ExpanderNode 类
    - 更新了 pom.xml，移除了相对路径配置
    - 删除了日志文件中的旧信息

---
+++ alibaba-graph/src/main/java/com/ustc/myy/alibabagraph/simple/config/GraphConfiguration.java
@@ -0,0 +1,51 @@
+package com.ustc.myy.alibabagraph.simple.config;
+
+import com.alibaba.cloud.ai.graph.GraphRepresentation;
+import com.alibaba.cloud.ai.graph.KeyStrategy;
+import com.alibaba.cloud.ai.graph.KeyStrategyFactory;
+import com.alibaba.cloud.ai.graph.StateGraph;
+import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import com.alibaba.cloud.ai.graph.state.strategy.ReplaceStrategy;
+import com.ustc.myy.alibabagraph.simple.node.ExpanderNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+
+import static com.alibaba.cloud.ai.graph.action.AsyncNodeAction.node_async;
+
+@Configuration
+public class GraphConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(GraphConfiguration.class);
+
+    @Bean
+    public StateGraph simpleGraph(ChatClient.Builder chatClientBuilder) throws GraphStateException {
+        KeyStrategyFactory keyStrategyFactory = () -> {
+            HashMap<String, KeyStrategy> keyStrategyHashMap = new HashMap<>();
+
+            // 用户输入
+            keyStrategyHashMap.put("query", new ReplaceStrategy());
+            keyStrategyHashMap.put("expander_number", new ReplaceStrategy());
+            keyStrategyHashMap.put("expander_content", new ReplaceStrategy());
+            return keyStrategyHashMap;
+        };
+
+        StateGraph stateGraph = new StateGraph(keyStrategyFactory)
+                .addNode("expander", node_async(new ExpanderNode(chatClientBuilder)))
+                .addEdge(StateGraph.START, "expander")
+                .addEdge("expander", StateGraph.END);
+
+        // 添加 PlantUML 打印
+        GraphRepresentation representation = stateGraph.getGraph(GraphRepresentation.Type.PLANTUML,
+                "expander flow");
+        logger.info("\n=== expander UML Flow ===");
+        logger.info(representation.content());
+        logger.info("==================================\n");
+
+        return stateGraph;
+    }
+}
